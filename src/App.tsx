9cc3043a5bad7cbfbb50b52c55715e56f7ad436d
Хлопцев Иван <ikhloptsev@gmail.com>
2022-12-14 19:58:14 +0300

    from https://www.kindacode.com/article/react-typescript-using-useref-hook/

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
+
 import './App.css';
 
 function App() {
+  //const [term, setTerm] = useState('');
+
+  const nameRef = useRef<HTMLInputElement | null>(null);
+  const emailRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => focusOnName, []);
+
+  // const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   console.log(term);
+  // };
+
+  const focusOnName = () =>  { nameRef.current?.focus() };
+  const focusOnEmail = () =>  { emailRef.current?.focus() };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {/* <form onSubmit={submitForm}>
+        <input 
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+          type='text'
+          placeholder='Enter a term'
+          className='input'
+        />
+        <button className='btn'>Submit</button>
+      </form> */}
+      <h1>Example</h1>
+      <input ref={nameRef} type='text' className='input' placeholder='Name'/>
+      <input ref={emailRef} type='text' className='input' placeholder='Email'/>
+
+      <div className='btn-wrapper'>
+        <button onClick={focusOnName} className='btn'>Focus on Name</button>
+        <button onClick={focusOnEmail} className='btn'>Focus on Email</button>
+      </div>
+      
     </div>
   );
 }
